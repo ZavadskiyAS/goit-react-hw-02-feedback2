b18780388497cb33dc96abaebd864ddbc868a2b8
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-07-02 19:43:21 +0300

    Updates

---
+++ precache-manifest.2b7680218d0dd2e284762f1b4d278df3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "689baddfcb324db686b7ed528d25f740",
+    "url": "/goit-react-hw-02-feedback2/index.html"
+  },
+  {
+    "revision": "8ac9a1a5e54f46f4eab4",
+    "url": "/goit-react-hw-02-feedback2/static/js/2.81969ca0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback2/static/js/2.81969ca0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8589b53d87fe9b7d3903",
+    "url": "/goit-react-hw-02-feedback2/static/js/main.dd908227.chunk.js"
+  },
+  {
+    "revision": "7a903d8c4193ff557611",
+    "url": "/goit-react-hw-02-feedback2/static/js/runtime-main.7ec33d0b.js"
+  }
+]);
